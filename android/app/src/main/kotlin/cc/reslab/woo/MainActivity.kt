a4806c2d3655e2c1c5b78b50e04bfe3bc80e66dc
Andrew A. Chuev <andrew.chuev@gmail.com>
2020-12-16 06:01:36 +0200

    update fix

---
+++ android/app/src/main/kotlin/cc/reslab/woo/MainActivity.kt
@@ -0,0 +1,6 @@
+package cc.reslab.woo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const logo = TextStyle(fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.white);
+
+const h1 = TextStyle(fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.black);
+const h2 = TextStyle(fontSize: 30.0, fontWeight: FontWeight.w500, color: Colors.black);
+const h3 = TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500, color: Colors.black);
+const h4 = TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400, color: Colors.black);
